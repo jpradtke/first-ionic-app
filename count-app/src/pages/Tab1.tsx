b20e5ce397053e7fb28e0dedcf2000c703f92a9a
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-10 14:55:45 +0200

    add count

--- count-app/src/pages/Tab1.tsx
+++ count-app/src/pages/Tab1.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import Count from "./count"
 
 const Tab1: React.FC = () => {
   return (
@@ -12,12 +13,17 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+      
         <IonHeader collapse="condense">
+          
+          
           <IonToolbar>
+          
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <Count />
       </IonContent>
     </IonPage>
   );
---
+++ count-app/src/pages/count.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+
+
+class Count extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            count : 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick (){
+        return(
+            this.setState(
+                prevState => {
+                    return{
+                            count: prevState.count + 1
+                        }
+                } )
+        )
+    }
+
+    render(){
+        return(
+            <div className = "gameDiv">
+                <h1>{this.state.count}</h1>
+                <IonButton className = "gameButton" onClick = {this.handleClick} >Count</IonButton>
+            </div>
+
+        )
+    }
+}
+
+export default Count
